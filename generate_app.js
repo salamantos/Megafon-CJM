828148fe7e21528e135091eba79c4c238f9f1872
m-malikov <malikov.ma@phystech.edu>
2018-05-06 07:57:01 +0300

    advanced editor and generation

--- generate_app.js
+++ generate_app.js
@@ -1,20 +1,27 @@
 function generateStep(stepData) {
-  var step = $('<div>', { id: stepData.id, class: "step" });
-  step.append($('<h5>', { class: "step-title", text: stepData.title }));
-  step.append($('<div>', { class: stepData.icon }));
+  var step = $('<div>', { class: "step d-flex flex-row align-items-center" });
+  var stepBody = $('<div>', { class: "step-body" });
+  stepBody.append($('<h5>', { class: "step-title", text: stepData.title }));
+  stepBody.append($('<div>', { class: stepData.icon }));
+  stepBody.on("click", function(evt) {
+    updateStep(this);
+  });
+
   
   if (stepData.smsInfo) {
-    step.append($('<div>', { class: "sms-info", text: stepData.smsInfo }));
+    stepBody.append($('<div>', { class: "sms-info", text: stepData.smsInfo }));
   }
 
   if (stepData.info) {
-    step.append($('<div>', { class: "info", text: stepData.info }));
+    stepBody.append($('<div>', { class: "info", text: stepData.info }));
   }
 
   if (stepData.danger) {
-    step.append($('<div>', { class: "danger", text: stepData.danger }));
+    stepBody.append($('<div>', { class: "danger", text: stepData.danger }));
   } 
 
+  step.append(stepBody);
+  var addButton = $('<button>', { class: "btn btn-light", text: '+'}));
   return step;
 }
 
@@ -28,7 +35,11 @@ function generateVariant(variantData) {
 
 function generateState(stateData) {
   var state = $('<div>', { class: "global-step" });
-  state.append($('<div>', { class: "header", text: stateData.name }));
+  var fb = $('<div>', { class: "d-flex flex-row" })
+  fb.append($('<div>', { class: "header", text: stateData.name }));
+  var addBtn = $('<button>', { class: "btn btn-light mr-auto", text: '+', click: addState, style: "display: inline-block" })
+  fb.append(addBtn);
+  state.append(fb);
   var variantsDiv = $('<div>', { class: "d-flex justify-content-around flex-column" });
   stateData.variants.forEach(function (variantData) {
     variantsDiv.append(generateVariant(variantData));
@@ -149,6 +160,109 @@ appData = {
   ] 
 }
 
+
+
+function getHeaderValue() {
+  var headerValue = $('input[name="titleRadios"]:checked').val();
+  if (headerValue == 'other')
+    return $('#title-input').val();
+  else 
+    return headerValue;
+}
+
+function updateHeader(header) {
+  $('#title-modal').modal('show');
+  $('#title-input').val(header.innerText);
+  $('#title-button').one("click", function() {
+    header.innerText = getHeaderValue();
+    $('#title-modal').modal('hide');
+  });
+
+  $('#title-delete').one("click", function() {
+    $(header).parent().parent().remove();
+  });
+}
+
+function updateStep(step) {
+  $('#step-modal').modal('show');
+  $('#step-title').val($(step).children('.step-title')[0].innerText);
+  var iconValue = $(step).children('.fa, .fas, .far')[0].className;
+  $('[name="icon"]').removeAttr('checked');
+  $("input[name=icon][value='" + iconValue + "']").prop('checked', true);
+  var infoDiv = $(step).children('.info')[0]; 
+  if (infoDiv)
+    $('#step-info').val(infoDiv.innerText);
+  else
+    $('#step-info').val("");
+  var smsInfoDiv = $(step).children('.sms-info')[0]; 
+  if (smsInfoDiv)
+    $('#step-sms-info').val(smsInfoDiv.innerText);
+  else
+    $('#step-sms-info').val("");
+  var dangerDiv = $(step).children('.danger')[0]; 
+  if (dangerDiv)
+    $('#step-danger').val(dangerDiv.innerText);
+  else
+    $('#step-danger').val("");
+
+  $('#step-button').one("click", function() {
+    addStep($(step).parent());
+    $(step).parent().remove();
+    $('#step-modal').modal('hide');
+  }); 
+
+  $('#step-delete').one("click", function() {
+    $(step).parent().remove();
+  });
+}
+
+function addStep(parentElement) {
+  console.log(parentElement);
+  var stepData = {
+    title: $('#step-title').val(),
+    icon: $('input[name="icon"]:checked').val(),
+    info: $('#step-info').val(),
+    smsInfo: $('#step-sms-info').val(),
+    danger: $('#step-danger').val(),
+  }
+  step = generateStep(stepData);
+  parentElement.after(generateStep(stepData));
+}
+
+function addState(evt) {
+  var stateData = {
+    name: '',
+    variants: [],
+  }
+  var state = generateState(stateData);
+  var header = $($(state).children()[0]).children()[0];
+  $(header).click(function(evt) {
+    updateHeader(evt.target);
+  });
+  updateHeader(header);
+  $(evt.target).parent().parent().after(state);
+}
+
+function deleteState(evt) {
+  $(evt.target).parent().remove();
+}
+
+
 $('document').ready(function() {
   generateApp(appData);
+  $('.header').click(function(evt) {
+    updateHeader(evt.target);
+  });
+  $.each(steps, function(index, value) {
+    var option = $('<a>', { class: 'dropdown-item', href: '#', text: value.title });
+    option.click(function() {
+      $('#step-title').val(value.title);
+      $('#step-info').val(value.info);
+      $('#step-sms-info').val(value.smsInfo);
+      $('#step-danger').val(value.danger);
+      $('[name="icon"]').removeAttr('checked');
+      $("input[name=icon][value='" + value.icon + "']").prop('checked', true);
+    });
+    $('#step-options').append(option);
+  });
 });
